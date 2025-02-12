f4a7171b68d6993df60712e5acdf6ca38b7074e7
federico <federicogaglio99@gmail.com>
2025-02-12 14:21:04 +0100

    inizio

---
+++ src/main/java/com/example/GifPoject/GifPojectApplication.java
@@ -0,0 +1,13 @@
+package com.example.GifPoject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GifPojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GifPojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/GifPoject/GifPojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GifPoject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GifPojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
