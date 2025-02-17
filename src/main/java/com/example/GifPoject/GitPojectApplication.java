cbdafbcd142b685c20ce5ba8b07ca6da38e95327
federico <federicogaglio99@gmail.com>
2025-02-17 09:52:37 +0100

    Primo commit: creazione del progetto

--- src/main/java/com/example/GifPoject/GitPojectApplication.java
+++ src/main/java/com/example/GifPoject/GitPojectApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GifPojectApplication {
+public class GitPojectApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(GifPojectApplication.class, args);
+		SpringApplication.run(GitPojectApplication.class, args);
 	}
 
 }
